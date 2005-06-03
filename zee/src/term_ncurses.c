0408509795aabc57ede23cc3e8d737b728b3fb2e
rrt <devnull@localhost>
2005-06-03 23:35:15 +0000

    Commit configure.ac changes to reinstate ncurses.
    Tidy up term_ncurses.c a bit, removing unused colour defines &c.

--- zee/src/term_ncurses.c
+++ zee/src/term_ncurses.c
@@ -3,24 +3,22 @@
    Copyright (c) 2003-2004 Reuben Thomas.
    All rights reserved.
 
-   This file is part of Zile.
+   This file is part of Zee.
 
-   Zile is free software; you can redistribute it and/or modify it under
+   Zee is free software; you can redistribute it and/or modify it under
    the terms of the GNU General Public License as published by the Free
    Software Foundation; either version 2, or (at your option) any later
    version.
 
-   Zile is distributed in the hope that it will be useful, but WITHOUT ANY
+   Zee is distributed in the hope that it will be useful, but WITHOUT ANY
    WARRANTY; without even the implied warranty of MERCHANTABILITY or
    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
    for more details.
 
    You should have received a copy of the GNU General Public License
-   along with Zile; see the file COPYING.  If not, write to the Free
+   along with Zee; see the file COPYING.  If not, write to the Free
    Software Foundation, 59 Temple Place - Suite 330, Boston, MA
    02111-1307, USA.  */
-
-/*	$Id$	*/
 
 #include "config.h"
 
@@ -35,18 +33,6 @@
 typedef SCREEN Screen;
 
 Screen *screen;
-
-#define ZILE_COLOR_BLACK0
-#define ZILE_COLOR_RED1
-#define ZILE_COLOR_GREEN2
-#define ZILE_COLOR_YELLOW3
-#define ZILE_COLOR_BLUE4
-#define ZILE_COLOR_MAGENTA5
-#define ZILE_COLOR_CYAN6
-#define ZILE_COLOR_WHITE7
-#define ZILE_COLOR_BLUEBG8
-
-extern int LINES, COLS;
 
 #include "main.h"
 #include "extern.h"
