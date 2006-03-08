ad49b2f58fbfec0256ca11a27db8afd3dbb79413
apt1002 <devnull@localhost>
2006-03-08 16:06:09 +0000

    We now have a working rblist_concat.
    rblist_from_array is now implemented as a primitive.
    
    I've added performance notes to the API documentation in rblist.h.

--- zee/src/rblist.c
+++ zee/src/rblist.c
@@ -20,6 +20,7 @@
    Software Foundation, Fifth Floor, 51 Franklin Street, Boston, MA
    02111-1301, USA.  */
 
+#include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -29,20 +30,23 @@
 #include "rblist.h"
 
 
-/* For debugging: incremented every time random_choice is called. */
-static size_t choice_counter = 0;
+/* For debugging: incremented every time random_float is called. */
+static size_t random_counter = 0;
 
-/*
- * Returns true with probability p/(p+q).
- * Assumes (p+q) is representable as a size_t.
- */
-static inline bool random_choice(size_t p, size_t q)
+/* Returns a random double in the range 0.0 (inclusive) to 1.0 (exclusive). */
+static inline double random_double(void)
 {
-  static size_t seed = 483568341; /* Arbitrary. */
+  static uint32_t seed = 483568341; /* Arbitrary. */
 
-  choice_counter++;
+  random_counter++;
   seed = (2 * seed - 1) * seed + 1; /* Maximal period mod any power of two. */
-  return (p + q) * (float)seed < p * ((float)SIZE_MAX + 1.0f);
+  return seed * (1.0 / (1.0 + (double)UINT32_MAX + 1.0));
+}
+
+/* Returns a random size_t in the range from 1 to 'n'. */
+static inline size_t random_one_to_n(size_t n)
+{
+  return 1 + (size_t)(n * random_double());
 }
 
 /*
@@ -50,7 +54,7 @@ static inline bool random_choice(size_t p, size_t q)
  * rblists shorter than this will be implemented using a primitive array.
  * Increase to use less memory and more CPU.
  */
-#define RBLIST_MINIMUM_NODE_LENGTH 32
+#define MINIMUM_NODE_LENGTH 32
 
 /*
  * In ML, this data structure would be defined as follows:
@@ -64,7 +68,7 @@ static inline bool random_choice(size_t p, size_t q)
  * is the length of the list.
  *
  * In C, we can distinguish "leaf" from "node" by comparing the length
- * with RBLIST_MINIMUM_NODE_LENGTH. We can therefore use an untagged
+ * with MINIMUM_NODE_LENGTH. We can therefore use an untagged
  * union. To allow us to access the length without first knowing whether
  * it is a leaf or a node, we include it as a third member of the union.
  *
@@ -97,47 +101,126 @@ union rblist {
   struct node node;
 };
 
-static struct leaf empty = {0};
+/* Static utility functions. */
 
-rblist rblist_empty = (rblist)&empty;
+/* The struct leaf to which rblist_empty points. */
+static const struct leaf empty = {0};
 
-rblist rblist_singleton(char c)
+/* Allocates and initialises a struct leaf. */
+static inline rblist leaf_from_array(const char *s, size_t length)
 {
-  struct leaf *ret = zmalloc(sizeof(struct leaf) + sizeof(char));
-  ret->length = 1;
-  ret->data[0] = c;
+  assert(length < MINIMUM_NODE_LENGTH);
+  struct leaf *ret = zmalloc(sizeof(struct leaf) + length * sizeof(char));
+  ret->length = length;
+  memcpy(&ret->data[0], s, length);
   return (rblist)ret;
 }
 
-rblist rblist_concat(rblist left, rblist right)
+/* Tests whether an rblist is a leaf or a node. */
+static inline bool is_leaf(rblist rbl)
 {
-  size_t new_length = left->length + right->length;
-  if (new_length < RBLIST_MINIMUM_NODE_LENGTH) {
-    struct leaf *ret = zmalloc(sizeof(struct leaf) + sizeof(char) * new_length);
-    ret->length = new_length;
-    memcpy(&ret->data[0], &left->leaf.data[0], left->length);
-    memcpy(&ret->data[left->length], &right->leaf.data[0], right->length);
-    return (rblist)ret;
+  return rbl->length < MINIMUM_NODE_LENGTH;
+}
+
+/*
+ * Used internally by rblist_concat.
+ *
+ * Splits 'rbl' into two halves and stores those halves in *left and
+ * *right. If is_leaf(rbl), the split is made at position 'pos', which
+ * must be chosen from a uniform probability distribution over
+ * 0 < pos < rbl->length. If !is_leaf(rbl), the split is made
+ * in the same place that 'rbl' was split last time, i.e. the two halves
+ * are rbl->node.left and rbl->node.right.
+ *
+ * Either way, the split happens at a random position chosen from a
+ * uniform probability distribution.
+ */
+static inline void random_split(rblist rbl, size_t pos, rblist *left, rblist *right)
+{
+  if (is_leaf(rbl)) {
+    *left = leaf_from_array(&rbl->leaf.data[0], pos);
+    *right = leaf_from_array(&rbl->leaf.data[pos], rbl->length - pos);
   } else {
-    struct node *ret = zmalloc(sizeof(struct node));
-    ret->length = new_length;
-    if (random_choice(left->length, right->length)) {
-      ret->left = left->node.left;
-      ret->right = rblist_concat(left->node.right, right);
-    } else {
-      ret->left = rblist_concat(left, right->node.left);
-      ret->right = right->node.right;
-    }
-    return (rblist)ret;
+    *left = rbl->node.left;
+    *right = rbl->node.right;
   }
 }
 
-rblist rblist_from_array(const char *s)
+/* Start of constructor functions. */
+
+/*
+ * This operation is unique in picking the entire node/leaf structure
+ * completely at random. It can therefore be thought of as defining the
+ * correct probability distribution for randomly balanced lists.
+ *
+ * It's not really a necessary primitive constructor, but the above
+ * property argues for implementing it as a primitive.
+ */
+rblist rblist_from_array(const char *s, size_t length)
 {
-  rblist ret = rblist_empty;
-  for (; *s; s++)
-    ret = rblist_concat(ret, rblist_singleton(*s));
-  return ret;
+  if (length == 0) return rblist_empty;
+  if (length < MINIMUM_NODE_LENGTH)
+    return leaf_from_array(s, length);
+  struct node *ret = zmalloc(sizeof(struct node));
+  ret->length = length;
+  size_t pos = random_one_to_n(length - 1);
+  ret->left = rblist_from_array(&s[0], pos);
+  ret->right = rblist_from_array(&s[pos], length - pos);
+  return (rblist)ret;
+}
+
+const rblist rblist_empty = (rblist)&empty;
+
+rblist rblist_singleton(char c)
+{
+  return rblist_from_array(&c, 1);
+}
+
+/*
+ * Balancing condition:
+ *
+ *   rblist_concat(rblist_from_array(left), rblist_from_array(right))
+ *
+ * has the same probability distribution as
+ *
+ *   rblist_from_array(concatenation_of_left_and_right)
+ *
+ * Strictly, left and right must be uncorrelated for this balancing
+ * condition to hold, but in practice concatenating a list with itself
+ * (for example) does not have disastrous consequences.
+ */
+rblist rblist_concat(rblist left, rblist right)
+{
+  /* FIXME: Rewrite using a while loop instead of recursion. */
+  #define llen (left->length)
+  #define rlen (right->length)
+  #define mid (left->length)
+  size_t new_len = llen + rlen;
+  if (new_len < MINIMUM_NODE_LENGTH) {
+    struct leaf *ret = zmalloc(sizeof(struct leaf) + sizeof(char) * new_len);
+    ret->length = new_len;
+    memcpy(&ret->data[0], &left->leaf.data[0], llen);
+    memcpy(&ret->data[mid], &right->leaf.data[0], rlen);
+    return (rblist)ret;
+  } else {
+    struct node *ret = zmalloc(sizeof(struct node));
+    ret->length = new_len;
+    size_t pos = random_one_to_n(new_len - 1);
+    if (pos < mid) {
+      random_split(left, pos, &ret->left, &left);
+      ret->right = rblist_concat(left, right);
+    } else if (pos > mid) {
+      random_split(right, pos - mid, &right, &ret->right);
+      ret->left = rblist_concat(left, right);
+    } else { /* pos == mid */
+      ret->left = left;
+      ret->right = right;
+    }
+    return (rblist)ret;
+  }
+  #undef llen
+  #undef rlen
+  #undef mid
 }
 
 /* char *rblist_to_string(rblist rbl) */
@@ -168,7 +251,7 @@ int main(void)
 /*   assert(!strcmp(rblist_to_string(rbl3), "ab")); */
 
   const char *s1 = "Hello, I'm longer than 32 characters! Whoooppeee!!!";
-  rbl1 = rblist_from_array(s1);
+  rbl1 = rblist_from_array(s1, strlen(s1));
 /*   assert(!strcmp(rblist_to_string(rbl1), s1)); */
 
   return EXIT_SUCCESS;
--- zee/src/rblist.h
+++ zee/src/rblist.h
@@ -23,20 +23,48 @@
 #ifndef RBLIST_H
 #define RBLIST_H
 
-/* The type of lists. Instances are immutable. */
+/*
+ * The type of lists. This is a pointer type. The structure it points to
+ * is opaque. You may assume the structure is immutable (const). No
+ * operation in this library ever modifies an rblist in place.
+ *
+ * Takes memory O(n) where 'n' is the length of the list. Typically about
+ * twice as much memory will be used as would be used for an ordinary
+ * C array.
+ */
 typedef const union rblist *rblist;
 
-/* The empty list. */
-rblist rblist_empty;
+/* Primitive constructors. */
 
-/* Make a list of length 1. */
+/*
+ * Make an rblist from an array. This can be achieved using
+ * rblist_singleton and rblist_concat, but this method is more efficient.
+ *
+ * Takes time O(n) where 'n' is the length of the list.
+ */
+rblist rblist_from_array(const char *s, size_t length);
+
+/*
+ * The empty list. There is only one empty list, so this is a constant,
+ * not a function.
+ */
+const rblist rblist_empty;
+
+/*
+ * Make a list of length 1. See also rblist_from_array.
+ *
+ * Takes time O(1).
+ */
 rblist rblist_singleton(char c);
 
-/* Concatenate two lists (non-destructive). */
+/*
+ * Concatenate two lists (non-destructively).
+ *
+ * Takes time O(log(n)) where 'n' is the length of the result.
+ */
 rblist rblist_concat(rblist left, rblist right);
 
-/* Make an rblist from a string. */
-rblist rblist_from_array(const char *s);
+/* End of primitive constructors. */
 
 /* Make a string from an rblist. */
 char rblist_get(rblist rbl, size_t index);
