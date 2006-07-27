27bbf3fed33b37ce9ecbf964aa4bd318a26d3162
rrt <devnull@localhost>
2006-07-27 13:45:57 +0000

    Rename --no-user-rc to --norc, like some other programs.
    Respace to make it easier to check the columns line up.

--- zee/src/tbl_opts.h
+++ zee/src/tbl_opts.h
@@ -7,9 +7,9 @@
  * N.B. The options' order must correspond to the code in main.c.
  */
 
-X("batch",	0, "            run non-interactively\n")
-X("eval",	1, " CMD         execute command CMD\n")
-X("load",	1, " FILE        load file of commands\n")
-X("no-user-rc",	0, "       do not load ~/." PACKAGE "rc\n")
-X("help",	0, "             display this help message and exit\n")
-X("version",	0, "          display version information and exit\n")
+X("batch", 0, "            run non-interactively\n")
+X("eval", 1, " CMD         execute command CMD\n")
+X("load", 1, " FILE        load file of commands\n")
+X("norc", 0, "             do not load ~/.zeerc\n")
+X("help", 0, "             display this help message and exit\n")
+X("version", 0, "          display version information and exit\n")
