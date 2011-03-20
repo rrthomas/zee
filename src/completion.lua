4d8f47de879ee2e643d23208b27d5dea32e21ffe
Reuben Thomas <rrt@sc3d.org>
2011-03-20 14:10:08 +0000

    Update copyright years.

--- src/completion.lua
+++ src/completion.lua
@@ -1,6 +1,6 @@
 -- Completion facility functions
 --
--- Copyright (c) 2007, 2009, 2010 Free Software Foundation, Inc.
+-- Copyright (c) 2007, 2009, 2010, 2011 Free Software Foundation, Inc.
 --
 -- This file is part of GNU Zile.
 --
