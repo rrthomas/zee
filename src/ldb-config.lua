48deda39147f658e5dd5a4db25516b081d726abe
Reuben Thomas <rrt@sc3d.org>
2011-05-13 23:19:01 +0100

    Apply tostring to output in ldb.

--- src/ldb-config.lua
+++ src/ldb-config.lua
@@ -1,7 +1,8 @@
 return {
   output = function (...)
+             local arg = list.map (tostring, {...})
              io.flush ()
-             io.write (...)
+             io.write (unpack (arg))
              io.flush ()
            end
 }
