531d6991fef8a8012f3fc885bab8cea3ad8ed066
Gary V. Vaughan <gary@vaughan.pe>
2011-07-31 19:37:30 +0700

    [lisp-tests] separate interactive tests by renaming test files
    Interactive tests can now be easily spotted, and coverage of
    interactive vs non-interactive tests is easier to see.

--- src/lisp-tests/interactive-cd.el
+++ src/lisp-tests/interactive-cd.el
@@ -1,4 +1,3 @@
 ; execute-extended-command "cd" ENTER ENTER
 ; save-buffer save-buffers-kill-emacs
-(set-variable 'isearch-nonincremental-instead nil)
 (execute-kbd-macro "\M-xcd\r\r\C-x\C-s\C-x\C-c")
