22ffb5ffdda55be62b36e3551cf20e404a9eab14
Gary V. Vaughan <gary@vaughan.pe>
2011-08-08 16:39:12 +0700

    remove reference to obsolete function in dotzile-extra.el

--- src/dotzile-extra.el
+++ src/dotzile-extra.el
@@ -6,7 +6,6 @@
 ;(global-set-key "\C-h"        'backward-delete-char)
 ;(global-set-key "\M-:"        'mark-paragraph)
 ;(global-set-key "\M-hf"       'describe-function)
-;(global-set-key "\M-hF"       'view-zile-FAQ)
 ;(global-set-key "\M-hk"       'describe-key)
 ;(global-set-key "\M-hw"       'where-is)
 ;(global-set-key "\M-hv"       'describe-variable)
