eee1c02c7481ef7caae1f9aa257e1addc8932491
Reuben Thomas <rrt@sc3d.org>
2012-01-04 17:01:23 +0000

    Update a copyright line.

--- src/point.lua
+++ src/point.lua
@@ -1,6 +1,6 @@
 -- Point facility functions
 --
--- Copyright (c) 2010 Free Software Foundation, Inc.
+-- Copyright (c) 2010, 2012 Free Software Foundation, Inc.
 --
 -- This file is part of GNU Zile.
 --
