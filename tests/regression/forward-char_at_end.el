c8a3a807d6ce6659c877f935e849eba31ee19c59
Reuben Thomas <rrt@sc3d.org>
2012-10-03 15:02:02 +0100

    tests: translate forward-char regression test to Lua; oops

--- tests/regression/forward-char_at_end.el
+++
@@ -1,3 +0,0 @@
-; end-of-buffer a forward-char
-; save-buffer save-buffers-kill-emacs
-(execute-kbd-macro "\M->a\C-f\C-x\C-s\C-x\C-c")
---
+++ tests/regression/forward-char_at_end.lua
@@ -0,0 +1,3 @@
+-- move-end-file a move-next-character
+-- file-save file-quit
+macro_play ("M->", "a", "C-f", "M-s", "C-M-q")
