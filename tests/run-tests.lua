73d30a72759a38e22b545c54f1be1dbe4c5da87c
Reuben Thomas <rrt@sc3d.org>
2012-10-04 19:10:45 +0100

    run-tests.lua: update require line to work with Lua 5.2

--- tests/run-tests.lua
+++ tests/run-tests.lua
@@ -17,7 +17,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require "posix"
+posix = require "posix"
 require "std"
 
 -- N.B. Tests that use macro-play must note that keyboard input
