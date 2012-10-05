24012a7d579afb3e283f4c2eec9954be1666fe78
Reuben Thomas <rrt@sc3d.org>
2012-10-05 03:31:15 +0100

    Remove insert-file.

--- tests/insert-file.lua
+++
@@ -1,3 +0,0 @@
-call_command ("insert-file", "insert-file.input")
-call_command ("file-save")
-call_command ("file-quit")
--- tests/interactive/insert-file.lua
+++
@@ -1,2 +0,0 @@
--- M-x insert-file "insert-file.input" RET file-save file-quit
-macro_play ("M-x", "i", "n", "s", "e", "r", "t", "-", "f", "i", "l", "e", "RET", "i", "n", "s", "e", "r", "t", "-", "f", "i", "l", "e", ".", "i", "n", "p", "u", "t", "RET", "M-s", "C-M-q")
