bc378bdd301ff7035ac2f86a98dcf920df6cbd1d
Reuben Thomas <rrt@sc3d.org>
2014-01-17 21:31:57 +0000

    Use mkrockspecs.lua to make rockspecs

---
+++ rockspecs.lua
@@ -0,0 +1,43 @@
+-- Rockspec data
+
+-- Variables to be interpolated:
+--
+-- package_name
+-- version
+
+local default = {
+  package = package_name,
+  version = version.."-1",
+  source = {
+    url = "git://github.com/rrthomas/"..package_name..".git",
+  },
+  description = {
+    summary = "Experimental lightweight editor",
+    detailed = [[
+      Zee is a lightweight editor. Zee is aimed at small footprint
+      systems and quick editing sessions (it starts up and shuts down
+      instantly).
+   ]],
+    homepage = "http://github.com/rrthomas/"..package_name.."/",
+    license = "GPLv3+",
+  },
+  dependencies = {
+    "lua == 5.2",
+    "stdlib >= 34",
+    "luaposix >= 5.1.23",
+    "lrexlib-gnu >= 2.7.1",
+    "alien >= 0.7.0",
+  },
+  build = {
+    type = "command",
+    build_command = "LUA=$(LUA) CPPFLAGS=-I$(LUA_INCDIR) autoreconf -i && ./configure --prefix=$(PREFIX) --libdir=$(LIBDIR) --datadir=$(LUADIR) && make clean && make",
+    install_command = "make install",
+    copy_directories = {},
+  },
+}
+
+if version ~= "git" then
+  default.source.branch = "v"..version
+end
+
+return {default=default, [""]={}}
